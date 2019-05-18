34628a5068d8c595c110482e77c65d2f8c73074e
Ion D. Filho <iondrimba@gmail.com>
2019-05-18 17:18:49 -0300

    Remove unused title

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,6 @@ import Home from './pages/home';
 import About from './pages/about';
 import './App.css';
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
 const App: React.FC = () => {
   return (
     <div className="App App-header">
