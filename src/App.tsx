c6cd6def1627f3cac4e1751af4a6b86fd386e006
Ion Drimba F <iondrimba@users.noreply.github.com>
2019-05-18 15:30:28 -0300

    Add styled components (#8)
    * Add styled components packages config
    
    * Add title sample
    
    * Update inital spec

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Title>Hello World</Title>
         <a
           className="App-link"
           href="https://reactjs.org"
