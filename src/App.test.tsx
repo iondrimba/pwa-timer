2974f7c1a63f511c8a24046cde459917020e05ba
Ion D. Filho <iondrimba@gmail.com>
2019-05-18 15:09:55 -0300

    Update inital spec

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
 import App from './App';
+
+const Button = styled.button`
+  color: red;
+`
+
+it('render styled button', () => {
+  const tree = renderer.create(<Button />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
