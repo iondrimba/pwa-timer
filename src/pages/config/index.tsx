0b36710fc968fe727cf251a84c4b148d4d04b148
Ion D. Filho <iondrimba@gmail.com>
2019-06-16 17:44:57 -0300

    Add button styles

--- src/pages/config/index.tsx
+++ src/pages/config/index.tsx
@@ -23,6 +23,34 @@ const PlayIcon = styled(Play)`
   margin-left: 4px;
 `
 
+const PlusMinusButton = styled.button`
+  background-color: transparent;
+  border: 0;
+  color: #fff;
+  font-size: 50px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0);
+  transition: background-color .3s, color .3s;
+
+  :hover, :focus {
+    background-color: rgba(255, 255, 255, 0.2);
+    color: #fff;
+  }
+
+  :focus {
+    outline: 0;
+  }
+`
+const PlusMinusButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  justify-content: space-evenly;
+`;
+
 const Config = () => {
   const { seconds, setMinutes, navigate, minutesIncrease, minutesDecrease, secondsIncrease, secondsDecrease } = useContext(Ctx);
 
@@ -37,16 +65,16 @@ const Config = () => {
         <Divider big>:</Divider>
         <Number big>{convertSecondsToString(seconds.toString()).sec}</Number>
       </NumbersWrapper>
-      <NumbersWrapper>
+      <PlusMinusButtonWrapper>
         <div>
-          <button type="button" aria-label="Increase minutes" onClick={minutesIncrease}>+</button>
-          <button type="button" aria-label="Decrease minutes" onClick={minutesDecrease}>-</button>
+          <PlusMinusButton type="button" aria-label="Increase minutes" onClick={minutesIncrease}>+</PlusMinusButton>
+          <PlusMinusButton type="button" aria-label="Decrease minutes" onClick={minutesDecrease}>-</PlusMinusButton>
         </div>
         <div>
-          <button type="button" aria-label="Increase seconds" onClick={secondsIncrease}>+</button>
-          <button type="button" aria-label="Decrease seconds" onClick={secondsDecrease}>-</button>
+          <PlusMinusButton type="button" aria-label="Increase seconds" onClick={secondsIncrease}>+</PlusMinusButton>
+          <PlusMinusButton type="button" aria-label="Decrease seconds" onClick={secondsDecrease}>-</PlusMinusButton>
         </div>
-      </NumbersWrapper>
+      </PlusMinusButtonWrapper>
       <Options>
         <PillButton onClick={() => setMinutes(1)} aria-label="set 1 min">1 min</PillButton>
         <PillButton onClick={() => setMinutes(3)} aria-label="set 3 min">3 min</PillButton>
