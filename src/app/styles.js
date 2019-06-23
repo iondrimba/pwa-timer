40229838c314dba5fc90a2dce442a327183a1b05
Ion D. Filho <iondrimba@gmail.com>
2019-06-23 17:17:09 -0300

    Remove local font due to flickering

--- src/app/styles.js
+++ src/app/styles.js
@@ -2,24 +2,6 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Theme from './theme';
 
 const GlobalStyle = createGlobalStyle`
-@font-face {
-  font-family: 'blue_highwayregular';
-  src: url('./fonts/blue_highway_rg-webfont.woff2') format('woff2'),
-       url('./fonts/blue_highway_rg-webfont.woff') format('woff');
-  font-display: swap;
-  font-weight: normal;
-  font-style: normal;
-
-}
-
-@font-face {
-  font-family: 'bebas_neueregular';
-  src: url('./fonts/bebas.woff2') format('woff2'),
-       url('./fonts/bebas.woff') format('woff');
-  font-weight: normal;
-  font-display: swap;
-  font-style: normal;
-}
 
 *, :after, :before {
   box-sizing: border-box;
